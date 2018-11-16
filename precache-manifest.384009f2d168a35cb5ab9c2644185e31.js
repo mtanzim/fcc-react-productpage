88125b0fb34252795707cbebf4f37864ab001cfa
Tanzim Mokammel <mtanzim@gmail.com>
2018-11-16 21:58:11 +0100

    Updates

---
+++ precache-manifest.384009f2d168a35cb5ab9c2644185e31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/fcc-react-productpage/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "1b9be0f5bbe7d92c49a47db2d7fe3a86",
+    "url": "/fcc-react-productpage/static/media/logo.1b9be0f5.PNG"
+  },
+  {
+    "revision": "82c5cdbeb008706ba36c",
+    "url": "/fcc-react-productpage/static/js/runtime~main.82c5cdbe.js"
+  },
+  {
+    "revision": "fc274632dc89e16896ac",
+    "url": "/fcc-react-productpage/static/js/main.fc274632.chunk.js"
+  },
+  {
+    "revision": "7437613a216bb0d07a60",
+    "url": "/fcc-react-productpage/static/js/1.7437613a.chunk.js"
+  },
+  {
+    "revision": "fc274632dc89e16896ac",
+    "url": "/fcc-react-productpage/static/css/main.2d578d6d.chunk.css"
+  },
+  {
+    "revision": "22b61884537c9114cb5de41dfa40912c",
+    "url": "/fcc-react-productpage/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fff0dc97ec8ab2fcdede9f96ff8548a3.js"
+  "/fcc-react-productpage/precache-manifest.384009f2d168a35cb5ab9c2644185e31.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/fcc-react-productpage/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
