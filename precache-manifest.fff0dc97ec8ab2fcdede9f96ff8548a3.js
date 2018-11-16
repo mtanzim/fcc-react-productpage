766c44d77d88708dfc8ea9ca4af7f13d8d7a79f0
Tanzim Mokammel <mtanzim@gmail.com>
2018-11-16 21:54:10 +0100

    Updates

---
+++ precache-manifest.fff0dc97ec8ab2fcdede9f96ff8548a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "1b9be0f5bbe7d92c49a47db2d7fe3a86",
+    "url": "/static/media/logo.1b9be0f5.PNG"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7b7a24f152592e796d39",
+    "url": "/static/js/main.7b7a24f1.chunk.js"
+  },
+  {
+    "revision": "7437613a216bb0d07a60",
+    "url": "/static/js/1.7437613a.chunk.js"
+  },
+  {
+    "revision": "7b7a24f152592e796d39",
+    "url": "/static/css/main.2d578d6d.chunk.css"
+  },
+  {
+    "revision": "ad843a6d7a0ba8a1f24f0420a888d993",
+    "url": "/index.html"
+  }
+];
